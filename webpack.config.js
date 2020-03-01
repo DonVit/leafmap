7ced1dac41b11a0735263a1c9d284ee7f6999cef
Vitalie Doni <vitalie.doni@gmail.com>
2020-03-01 09:19:01 +0200

    add copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,7 +9,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({template: 'index.html'}), new MiniCssExtractPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({template: 'index.html'}), 
+    new MiniCssExtractPlugin(),
+    new CopyPlugin([
+      { from: 'node_modules/leaflet/dist/images', to: 'images' },
+    ]),
+  ],
   module: {
       rules: [
       {
